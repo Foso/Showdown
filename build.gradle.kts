ffcd301aedfa3c901f0a16a388c06721a6c02470
Jens Klingenberg <mail@jensklingenberg.de>
2022-10-25 14:55:40 +0200

    build: downgrade Kotlin because Compose for Web isnt compatible

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("org.jetbrains.kotlin.multiplatform") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "1.7.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
